bb6cc301038df0a439eca9b741dc24793d9455d1
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-09-27 17:23:48 -0300

    Module 03[08:10]

--- scripts/module03.sh
+++ scripts/module03.sh
@@ -26,28 +26,38 @@ do
 done && echo -e "## Step 08 done!\n" && sleep 0.2
 #_______________________________________________________________________________________________________________________________
 
-## M09: scov table creation
-#mkdir -p $SCOV
-#for FSCOV in $FILTHIT/*filt.tsv;
-#do
-#	sed '1d' $FSCOV |
-#	awk '{$16=int(($4/$6)*100)}1' |
-#	sed '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq\tscov' |
-#	awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$16,$10,$11,$12,$13,$14,$15 }' |
-#	sed 's/ /\t/g' > ${FSCOV%_mhit_filt.tsv}_scov.tsv &&
-#	mv $FILTHIT/*scov.tsv $SCOV
-#done
-## _______________________________________________________________________________________________________________________________
-#
-## M10: get a representative sequence (q/scov>=70)
-#mkdir -p $QSCOV70
-#for QSCOV in $SCOV/*scov.tsv
-#do
-#	$FQCOV $QSCOV ${QSCOV%_scov.tsv}_qscov70.tsv &&
-#	mv $SCOV/*qscov70.tsv $QSCOV70
-#done
-##_______________________________________________________________________________________________________________________________
-#
+# M09: scov table creation
+checkIfDir "$FILTHIT"
+echo -e "\e[01m## Step 09\e[0m"
+echo -e "## Creating scov column:"
+mkdir -p $SCOV
+for FSCOV in $FILTHIT/*filt.tsv;
+do
+    checkIfFile $FSCOV $FILTHIT
+    echo "   $(basename $FSCOV)"
+    sed '1d' $FSCOV |
+	awk '{$16=int(($4/$6)*100)}1' |
+	sed '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq\tscov' |
+	awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$16,$10,$11,$12,$13,$14,$15 }' |
+	sed 's/ /\t/g' > ${FSCOV%_mhit_filt.tsv}_scov.tsv &&
+	mv $FILTHIT/*scov.tsv $SCOV
+done && echo -e "## Step 09 done!\n" && sleep 0.2
+# _______________________________________________________________________________________________________________________________
+
+# M10: get a representative sequence (q/scov>=70)
+checkIfDir "$SCOV"
+echo -e "\e[01m## Step 10\e[0m"
+echo -e "## Filtering by scov:"
+mkdir -p $QSCOV70
+for QSCOV in $SCOV/*scov.tsv
+do
+    checkIfFile $QSCOV $SCOV
+    echo "   $(basename $QSCOV)"
+    $FQCOV $QSCOV ${QSCOV%_scov.tsv}_qscov70.tsv &&
+	mv $SCOV/*qscov70.tsv $QSCOV70
+done && echo -e "## Step 10 done!\n" && sleep 0.2
+#_______________________________________________________________________________________________________________________________
+
 ## M11: get sequences from list of ids
 #mkdir -p $IDSEQ
 #mkdir -p $TMPIDS
