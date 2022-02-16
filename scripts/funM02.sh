0bda3660dca5bebf14b2df673db7270154c1cef6
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-02-16 15:53:25 -0300

    Module 02/step03 working

--- scripts/funM02.sh
+++ scripts/funM02.sh
@@ -11,3 +11,11 @@ else
     exit 1
 fi
 #_______________________________________________________________________________________________________________________________
+step03 () {
+    INARQ03=$1
+    mkdir -p $SELFBLAST
+    checkBlast
+    echo "   $(basename $INARQ03)"
+	  $BLAST -query $INARQ03 -subject $INARQ03 -max_hsps 5 -max_target_seqs 5 -outfmt "7 qseqid sseqid pident length qlen slen evalue bitscore qcovs qstart qend sstart send qseq sseq" -out ${INARQ03%_length.fa}_blastout.tsv || exit 1 &&
+	  mv $LONGLEN/*blastout.tsv $SELFBLAST
+}
--- scripts/module02.sh
+++ scripts/module02.sh
@@ -14,38 +14,30 @@ fi
 #_______________________________________________________________________________________________________________________________
 
 # M06: Selfblast
-checkIfDir "$LONGLEN"
 echo -e "\e[01m### STARTING MODULE 02 ###\e[0m"
-echo -e "\e[01m## Step 06\e[0m"
-checkBlast
+echo -e "\e[01m## Step 03\e[0m"
 echo -e "## Selfblasting file:"
-## Create directory
-mkdir -p $SELFBLAST
-for CLEANFA in $LONGLEN/*length.fa
-do
-    checkIfFile $CLEANFA $LONGLEN
-    echo "   $(basename $CLEANFA)"
-	  $BLAST -query $CLEANFA -subject $CLEANFA -outfmt "7 qseqid sseqid pident length qlen slen evalue bitscore qcovs qstart qend sstart send qseq sseq" -out ${CLEANFA%_length.fa}_blastout.tsv || exit 1 &&
-	  mv $LONGLEN/*blastout.tsv $SELFBLAST
-done && echo -e "## Step 06 done!\n" && sleep 0.2
+    step03 "$LONGLEN/$FPCONTROL"_length.fa
+    step03 "$LONGLEN/$FPTREATMENT"_length.fa
+echo -e "## Step 03 done!\n" && sleep 0.2
 #_______________________________________________________________________________________________________________________________
 
-# M07: separate onehit files from multihit files
-checkIfDir "$SELFBLAST"
-echo -e "\e[01m## Step 07\e[0m"
-echo -e "## Selecting onehit and multihit:"
-mkdir -p $ONEHIT $MULTIHIT
-for FSBLAST in $SELFBLAST/*blastout.tsv;
-do
-    checkIfFile $FSBLAST $SELFBLAST
-    echo "   $(basename $FSBLAST)"
-	# Cria arquivo com sequências com só um hit (elas mesmas)
-	grep "# 1 hits found" -A1 $FSBLAST | sed '/^--$/d;/^#/d' > ${FSBLAST%_blastout.tsv}_onehit.tsv &&
-	sed -i '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq' ${FSBLAST%_blastout.tsv}_onehit.tsv &&
-	mv $SELFBLAST/*onehit.tsv $ONEHIT
-	# Cria arquivo com as sequências com mais de um hit
-	sed '/# 1 hits found/,+1 d;/^# /d' $FSBLAST > ${FSBLAST%_blastout.tsv}_multihit.tsv &&
-	sed -i '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq' ${FSBLAST%_blastout.tsv}_multihit.tsv &&
-	mv $SELFBLAST/*multihit.tsv $MULTIHIT
-done && echo -e "## Step 07 done!\n" && sleep 0.2
-#_______________________________________________________________________________________________________________________________
+# # M07: separate onehit files from multihit files
+# checkIfDir "$SELFBLAST"
+# echo -e "\e[01m## Step 07\e[0m"
+# echo -e "## Selecting onehit and multihit:"
+# mkdir -p $ONEHIT $MULTIHIT
+# for FSBLAST in $SELFBLAST/*blastout.tsv;
+# do
+#     checkIfFile $FSBLAST $SELFBLAST
+#     echo "   $(basename $FSBLAST)"
+# 	# Cria arquivo com sequências com só um hit (elas mesmas)
+# 	grep "# 1 hits found" -A1 $FSBLAST | sed '/^--$/d;/^#/d' > ${FSBLAST%_blastout.tsv}_onehit.tsv &&
+# 	sed -i '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq' ${FSBLAST%_blastout.tsv}_onehit.tsv &&
+# 	mv $SELFBLAST/*onehit.tsv $ONEHIT
+# 	# Cria arquivo com as sequências com mais de um hit
+# 	sed '/# 1 hits found/,+1 d;/^# /d' $FSBLAST > ${FSBLAST%_blastout.tsv}_multihit.tsv &&
+# 	sed -i '1i qseqid\tsseqid\tpident\tlen\tqlen\tslen\tevalue\tbitscore\tqcovs\tqstart\tqend\tsstart\tsend\tqseq\tsseq' ${FSBLAST%_blastout.tsv}_multihit.tsv &&
+# 	mv $SELFBLAST/*multihit.tsv $MULTIHIT
+# done && echo -e "## Step 07 done!\n" && sleep 0.2
+# #_______________________________________________________________________________________________________________________________
