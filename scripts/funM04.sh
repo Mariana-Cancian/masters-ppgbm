55623d9732caa8e666ddd3dcb0d9f9a13320403d
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-03-16 16:15:01 -0300

    Module 04 done!

--- scripts/funM04.sh
+++ scripts/funM04.sh
@@ -13,5 +13,20 @@ fi
 #_______________________________________________________________________________________________________________________________
 
 step10 () {
-  
+  INARQ101=$1
+  INARQ102=$2
+  OUTARQ10=$3
+  mkdir -p $SAMPLEBLAST
+  echo "   $(basename $INARQ101) vs $(basename $INARQ102)"
+  $BLAST -query $INARQ101 -subject $INARQ102 -outfmt "7 qseqid sseqid pident length qlen slen evalue bitscore qcovs qstart qend sstart send qseq sseq" -out $OUTARQ10
+  mv $FILTER3/*.tsv $SAMPLEBLAST
+}
+
+step11 () {
+  INARQ111=$1
+  INARQ112=$2
+  OUTARQ11=$3
+  mkdir -p $RBHBLAST
+  python2 $FRBCT $INARQ111 $INARQ112 1 2 10 high $OUTARQ11 &&
+  mv "$SAMPLEBLAST/$RBH_RES" $RBHBLAST
 }
