68cdec17f6b2e14cbc3e40fc895b508f7e35b73f
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-06-07 21:26:15 -0300

    Try/except block fixed

--- scripts/M05_check_nt_vanilla.py
+++ scripts/M05_check_nt_vanilla.py
@@ -12,26 +12,26 @@ nts = ['A','C','G','T']
 # Desire length
 seqLen = 25
 # Input file
-seqri = sys.argv[1]
-
-# Read file and add to dictionary
 try:
-    with open(seqri, 'r+') as read_file:
-        for line in read_file.readlines():
-            if line.startswith('>'):
-                current_key = line[1:].strip()
-                # Adiciona os IDs ao dicionário. dict[key] = value
-                seqs_by_ids[current_key] = ''
-            else:
-                # Adiciona as sequências ao dicionário.
-                seqs_by_ids[current_key] += line.upper().strip()
-
-    # Check if at least one of each nucleotide is in sequence and length
-    for i,s in seqs_by_ids.items():
-        if all(nt in s for nt in nts):
-            if len(s) > seqLen:
-                print(f'>{i}')
-                print(s)
+    seqri = sys.argv[1]
 except FileNotFoundError as e:
     print(e)
     exit()
+
+# Read file and add to dictionary
+with open(seqri, 'r+') as read_file:
+    for line in read_file.readlines():
+        if line.startswith('>'):
+            current_key = line[1:].strip()
+            # Adiciona os IDs ao dicionário. dict[key] = value
+            seqs_by_ids[current_key] = ''
+        else:
+            # Adiciona as sequências ao dicionário.
+            seqs_by_ids[current_key] += line.upper().strip()
+
+# Check if at least one of each nucleotide is in sequence and length
+for i,s in seqs_by_ids.items():
+    if all(nt in s for nt in nts):
+        if len(s) > seqLen:
+            print(f'>{i}')
+            print(s)
--- scripts/fastq_to_fasta.py
+++ scripts/fastq_to_fasta.py
@@ -11,12 +11,14 @@ except ModuleNotFoundError:
 try:
 # Input (fastq file)
     FASTQ=sys.argv[1]
-    FASTA=os.path.splitext(FASTQ)[0]
-    FASTA=f'{FASTA}.fa'
-    # Convert
-    #print('Converting...')
-    SeqIO.convert(FASTQ,'fastq',FASTA,'fasta')
 except (IndexError, FileNotFoundError):
-    sys.exit("File(s) missing.")
+    print("File(s) missing.")
+    exit()
+
+FASTA=os.path.splitext(FASTQ)[0]
+FASTA=f'{FASTA}.fa'
+# Convert
+#print('Converting...')
+SeqIO.convert(FASTQ,'fastq',FASTA,'fasta')
 
 #print('Done!')
